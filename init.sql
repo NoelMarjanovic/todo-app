317569deee6b1a7bc51383dde963c23a0e51c79d
Noel <noel.marjanovic@pax.ch>
2023-01-30 10:06:44 +0100

    Added Base

---
+++ init.sql
@@ -0,0 +1,5 @@
+create table todo (
+                      id int primary key AUTO_INCREMENT,
+                      title varchar(50) not null,
+                      description varchar(255) not null
+);
